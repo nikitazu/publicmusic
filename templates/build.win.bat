1e3734d4a278c19661a6326aa6613dd210134a49
Nikita Zuev <nikitazu@gmail.com>
2016-01-18 23:12:27 +0300

    Build file for Windows - WAV conversion step
    Requires timidity executable with preconfigured soundfont in D:\apps\mus\timidity213 directory.
    Maybe I should really do it in a portable way... but not today.
    
    Also ignore *.wav files.

--- templates/build.win.bat
+++ templates/build.win.bat
@@ -1,3 +1,13 @@
+echo off
 rem Creates PDFs and MIDIs for every lilypond file (*.ly) in the current folder
+
 set lily="%PROGRAMFILES(X86)%\LilyPond\usr\bin\lilypond.exe"
-%lily% --pdf *.ly > work.log 2>&1
+set timidity="D:\apps\mus\timidity213\timidity.exe"
+
+echo ############ LILYPOND >work.log
+%lily% --pdf *.ly >>work.log 2>&1
+
+echo ############ TIMIDITY >>work.log
+%timidity% --output-stereo -Ow *.mid >> work.log 2>&1
+
+echo ############ DONE >>work.log
